c776fc54952af99b0dd72293cd8e68af50ed8dd7
CTK4 <taylorkogan@gmail.com>
2026-02-15 17:03:55 -0500

    Update Interviews.tsx

--- src/pages/Interviews.tsx
+++ src/pages/Interviews.tsx
@@ -1,6 +1,4 @@
--import { useState } from "react";
--import { useEffect, useMemo, useState } from "react";
-+import { useEffect, useMemo, useState } from "react";
+ import { useEffect, useMemo, useState } from "react";
  import { useNavigate } from "react-router-dom";
  import { useGame, type InterviewResult } from "@/context/GameContext";
  import { getTeamById } from "@/data/leagueDb";
